c5bfdefa0e6c1765af95c41ac3f9d0d38c09e442
david_li <none@none>
2007-06-20 15:37:49 +0000

    deleted addEvent function since it already exists in tabulate.js

--- chrome/content/js/sorttable.js
+++ chrome/content/js/sorttable.js
@@ -192,20 +192,3 @@ function ts_sort_default(a,b) {
   if (aa<bb) return -1;
   return 1;
 }
-
-
-function addEvent(elm, evType, fn, useCapture)
-// addEvent and removeEvent
-// cross-browser event handling for IE5+,  NS6 and Mozilla
-// By Scott Andrew
-{
-  if (elm.addEventListener){
-    elm.addEventListener(evType, fn, useCapture);
-    return true;
-  } else if (elm.attachEvent){
-    var r = elm.attachEvent("on"+evType, fn);
-    return r;
-  } else {
-    alert("Handler could not be removed");
-  }
-} 

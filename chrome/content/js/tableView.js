984f790d927e092710f83d112a4edc2d0ce67402
david_li <none@none>
2007-07-03 17:43:06 +0000

    formatting stuff

--- chrome/content/js/tableView.js
+++ chrome/content/js/tableView.js
@@ -1,7 +1,6 @@
-function tableView(container,doc) {
-    var nv; 
-    //The vars specific to a tableView
-    // global to this function since I need it in addRow
+function tableView(container,doc) 
+{
+    var nv; // made this global since I need it later
     var activeSingleQuery = null;
     
     this.document=null;
@@ -11,26 +10,25 @@ function tableView(container,doc) {
       this.document=document;
     
     //The necessary vars for a View...
-    this.name="Table";        //Display name of this view.
-    this.queryStates=[];          //All Queries currently in this view.
-    this.container=container; //HTML DOM parent node for this view.
+    this.name="Table";               //Display name of this view.
+    this.queryStates=[];            //All Queries currently in this view.
+    this.container=container;       //HTML DOM parent node for this view.
     this.container.setAttribute('ondblclick','tableDoubleClick(event)')
     // this.container.setAttribute('onclick', 'onClickCell');
     
-    // q is a query object 
-    this.drawQuery = function (q) {
-        //alert(q);
-        //alert(q.pat);
-        //alert(q.vars);
-        //alert(q.orderBy);
-        //alert(q.name);
-        //alert(q.id);
-        this.onBinding = function (bindings) {
+    //----------------- this.drawQuery ----------------//
+    this.drawQuery = function (q) 
+    {
+        // q is a query object
+        //------------------ this.onBinding ----------------//
+        this.onBinding = function (bindings) 
+        {
             var i, tr, td;
             tabulator.log.info("making a row w/ bindings " + bindings);
             tr = this.document.createElement('tr');
             t.appendChild(tr);
-            for (i=0; i<nv; i++) {
+            for (i=0; i<nv; i++) 
+            {
                 v = q.vars[i];
                 //alert("calling matrixTD");
                 //alert(matrixTD(bindings[v]).innerHTML);
@@ -38,82 +36,116 @@ function tableView(container,doc) {
                 tr.appendChild(matrixTD(bindings[v]));
             } //for each query var, make a row
         }
+        //------------------ End this.onBinding ----------------//
+        
         var i, td, th, j, v;
         var t = this.document.createElement('table');
         var tr = this.document.createElement('tr');
-        nv = q.vars.length; // made this global b/c i need it in the addRows function
+        
+        nv = q.vars.length;
         
         t.appendChild(tr);
-        t.setAttribute('class', 'results sortable'); //changed back to 'results sortable'!?
-        t.setAttribute('id', 'tabulated_data'); //needed to make sortable
+        t.setAttribute('class', 'results sortable'); //needed to make sortable
+        t.setAttribute('id', 'tabulated_data'); 
+        
         emptyNode(this.container).appendChild(t); // See results as we go
-        for (i=0; i<nv; i++) {
-            v = q.vars[i]; //'v0?'
-            //alert(q.vars);
-            //alert(q);
-            //alert(v);
-            //alert(v.label);
+        
+        for (i=0; i<nv; i++) 
+        {
+            v = q.vars[i]; 
             //tabulator.log.debug("table header cell for " + v + ': '+v.label)
             th = this.document.createElement('th');
             th.appendChild(this.document.createTextNode(v.label));
             tr.appendChild(th);
         }
+        
         drawExport();
         drawAddRow();
-        //alert(kb.query(q, this.onBinding, myFetcher));
+        sortables_init();
+
         kb.query(q, this.onBinding, myFetcher);
         activeSingleQuery = q;
         this.queryStates[q.id]=1;
-        sortables_init();
        
-        // alert (container.innerHTML);
-        // This is the eventListener for Table Editing
+        //---------------- Table Editing-------------------//
         this.document.getElementById('tabulated_data').addEventListener('click', onClickCell, false);
 
-        function onClickCell(e) {
+        function onClickCell(e) 
+        {
             var srcElem = getTarget(e);
             if (srcElem.tagName != "TD") return;
+            srcElem.style.color = "
             else onEdit(e);
         }
 
         //Use getTdNode to avoid div and span elements
-        function getTDNode(e) {
+        function getTDNode(e) 
+        {
             var srcElem = getTarget(e);
             var tdNode = ancestor(srcElem, "TD");
             return tdNode;
         }
 
-        function onEdit(e) {
+        function onEdit(e) 
+        {
             var tdNode = getTDNode(e);
             //alert(tdNode.className);
             var oldTxt = tdNode.innerHTML;
-            var inputObj = document.createElement('INPUT');
+            var input = createInputForm(oldTxt);
+            var parent;
+            var newTD;
+            
+            if (!oldTxt)
+                input = createInputForm(" ");
             if (literalNodeTD(tdNode)) return;
-            if (tdNode.firstChild && tdNode.firstChild.tagName == "INPUT") return;
+            if (tdNode.firstChild && tdNode.firstChild.tagName == "INPUT")
+                return;
+            alert(tdNode.firstChild);
+            if (tdNode.firstChild) // node of the form <td> text </td>
+            {
+                tdNode.replaceChild(input, tdNode.firstChild);
+                input.select();
+            }
+            else // we have a node of the form <td />
+            {
+                parent = tdNode.parentNode;
+                newTD = document.createElement('TD');
+                parent.replaceChild(newTD, tdNode);
+                newTD.appendChild(input);
+            }
+        }
+        
+        function createInputForm(oldText) 
+        {
+            var inputObj = document.createElement('INPUT');
             inputObj.style.width = "99%";
-            inputObj.value = oldTxt;
+            inputObj.value = oldText;
             inputObj.type = "TEXT";
             inputObj.addEventListener ("blur", focusLost, false);
             inputObj.addEventListener ("keypress", checkForEnter, false);
-            tdNode.replaceChild(inputObj, tdNode.firstChild);
-            inputObj.select();
+            return inputObj;
         }
 
-        function focusLost(e) {
+        function focusLost(e) 
+        {
             var srcElem = getTarget(e);
             srcElem.parentNode.innerHTML = srcElem.value;
-            //Add code here to handle SPARQL query.  Make a call to clearInputAndSave.  
+            // Add code here to handle SPARQL query.  Make a call to clearInputAndSave.  
         }
 
-        function checkForEnter(e) {
+        function checkForEnter(e) 
+        {
             if (e.keyCode == 13) focusLost (e);
         }
+        //---------------- End Table Editing-------------------//
         
 /*        
-        document.getElementById('tabulated_data').addEventListener('mousemove', tableResize_OnMouseMoveBefore, false);
-        document.getElementById('tabulated_data').addEventListener('mousedown', tableResize_OnMouseDown, false);
-        
-        
+//---------------- Table Resize -------------------//       
+
+document.getElementById('tabulated_data').addEventListener('mousemove', tableResize_OnMouseMoveBefore, false);
+document.getElementById('tabulated_data').addEventListener('mousedown', tableResize_OnMouseDown, false);
+window.addEventListener("load", tableResize_CreateResizeBar,0);
+
 var resizeElement = "TH"; //Elements to be resized
 var edgeThreshold = 2;
 var rBarID = "rBar";
@@ -125,9 +157,11 @@ var sizeX = null;
 var adjacentCell = null;
 
 //Creates rBar on load 
-function tableResize_CreateResizeBar() {
+function tableResize_CreateResizeBar() 
+{
     var objItem = document.getElementById(rBarID);
-    if(!objItem) {
+    if(!objItem) 
+    {
         objItem = document.createElement("DIV");
         objItem.id = rBarID;
         objItem.style.position = "absolute";
@@ -139,22 +173,21 @@ function tableResize_CreateResizeBar() {
         document.body.appendChild(objItem);
     }
 }
-window.addEventListener("load", tableResize_CreateResizeBar,0);
 
-function tableResize_GetHeader(objReference) {
+function tableResize_GetHeader(objReference) 
+{
     var oElement = objReference;
-    if(oElement.tagName.toUpperCase() == resizeElement) {
+    if(oElement.tagName.toUpperCase() == resizeElement)
         return oElement; //print alert?
-    }
     return null;
 }
 
-function tableResize_CleanUp() {
+function tableResize_CleanUp() 
+{
     //document.getElementById('debug').value += 'Entered CleanUp\n';
     var rBar = document.getElementById(rBarID);
-    if(rBar) {
+    if(rBar) 
         rBar.style.display = "none";
-    }
     endX = null;
     sizeX = null;
     startX = null;
@@ -163,26 +196,25 @@ function tableResize_CleanUp() {
     return true;
 }
 
-function tableResize_OnMouseMoveBefore (event) {
+function tableResize_OnMouseMoveBefore (event) 
+{
     //document.getElementById('debug').value += 'Entered OnMouseMoveBefore\n';
     var objTH = tableResize_GetHeader(event.target); if (!objTH ) return;
 
     //document.getElementById('coords').value += "event.clientX " + event.clientX + "\n";
     //document.getElementById('coords').value += "event.layerX " + event.layerX + "\n";
     //document.getElementById('coords').value += "offsetWidth " + objTH.offsetWidth+ "\n";
-    if (event.layerX >= (objTH.offsetWidth - edgeThreshold)) {
+    if (event.layerX >= (objTH.offsetWidth - edgeThreshold)) 
         objTH.style.cursor = "e-resize";
-    }
-    else {
+    else 
         objTH.style.cursor = "";
-    }
     return true;
 }
 
-////////////////////// HERE"S THE IMPORTANT CODE  /////////////////////////////
-//// In order to use layerX, each TH must have style="position: relative"; ////
+// In order to use layerX, each TH must have style="position: relative"; 
 
-function tableResize_OnMouseDown(event) {
+function tableResize_OnMouseDown(event) 
+{
     //document.getElementById('debug').value += 'Entered OnMouseDown\n';
     objTable = this;
     var objTH = tableResize_GetHeader(event.target); if (!objTH) return;
@@ -193,7 +225,8 @@ function tableResize_OnMouseDown(event) {
     
     //IT DOESN"T SEEM TO GET PAST THIS STEP!
 
-    if ((objTH.tagName.toUpperCase() == resizeElement) && (objTH.style.cursor == "e-resize")) { 
+    if ((objTH.tagName.toUpperCase() == resizeElement) && (objTH.style.cursor == "e-resize")) 
+    { 
         startX = event.clientX;
         resizeTarget = objTH;
         
@@ -209,7 +242,8 @@ function tableResize_OnMouseDown(event) {
 }
 
 
-//function tableResize_HeaderProblem (event) {
+//function tableResize_HeaderProblem (event) 
+{
     //document.getElementById('debug').value += 'Entered HeaderProblem\n';
    //document.getElementById("tabulated_data").removeEventListener("mousemove", tableResize_OnMouseMoveAfter, true);
 //}
@@ -217,14 +251,16 @@ function tableResize_OnMouseDown(event) {
 
 // doesn't seem like I ever enter OnMouseAfter
 
-function tableResize_OnMouseMoveAfter(event) {
+function tableResize_OnMouseMoveAfter(event) 
+{
     //document.getElementById('debug').value += 'Entered OnMouseMoveAfter\n';
     rBar.style.left = event.clientX + window.pageXOffset;
     //document.getElementById("coords").value += rBar.style.left + "\n"; 
     event.stopPropagation();
 }
 
-function tableResize_OnMouseUp(event) {
+function tableResize_OnMouseUp(event) 
+{
     //document.getElementById('debug').value += 'Entered OnMouseUp\n';
     var iAdjCellOldWidth = 0;
     var iResizeOldWidth = 0;
@@ -234,9 +270,9 @@ function tableResize_OnMouseUp(event) {
     var adjacentCell = resizeTarget.nextSibling.nextSibling;
     //alert(adjacentCell);
     
-    //--------------------------------------
+    //---------------------------------------------------------
     // set the width on resizeTarget
-    // the distance that resizeTarget moves is startX+endX
+    // the distance that resizeTarget moves is startX+endX ----
     var endX = event.clientX;
     // startX is already defined
     var distanceMoved = endX - startX;
@@ -248,9 +284,9 @@ function tableResize_OnMouseUp(event) {
     resizeTarget.style.width = oldCellWidth + distanceMoved;
     //document.getElementById("coords").value += "newCellWidth " + newCellWidth + "\n"; 
     
-    //-------------------------------------
+    //-------------------------------------------------
     // set the width on adjacent Cell
-    // the distance that the adjacentCell moves is 
+    // the distance that the adjacentCell moves is ---- 
     var oldAdjacentWidth = adjacentCell.offsetWidth;
     adjacentCell.style.width = oldAdjacentWidth - distanceMoved;
     
@@ -260,10 +296,13 @@ function tableResize_OnMouseUp(event) {
     tableResize_CleanUp();
 }*/
 // Write getAdjacentCell function
-
-    } //this.drawQuery
-    //alert(this.document);
-    function drawAddRow () {
+//---------------- End Table Resize -------------------//
+    }
+//-------------- End drawQuery ------------------//
+    
+    //--------------- Add Row -----------------//
+    function drawAddRow () 
+    {
         var form = this.document.createElement('form');
         var but = this.document.createElement('input');
         form.setAttribute('textAlign','right');
@@ -276,25 +315,23 @@ function tableResize_OnMouseUp(event) {
     }
     
     
-    function addRow () {
+    function addRow () 
+    {
         // find number of columns in current table
         // create new tr and td elements, set class as editable
         // append these to the table
         // see if editing works
-        
         // other variables used: nv
-        
         // this should give you a list of all the tabulator project developers in the main tabulator file
         // something matching the q bindings
         // replace that with a selection bar?
-        
-        
         var i;
         var td;
         var tr = document.createElement('tr');
         var t = document.getElementById('tabulated_data');
         var lastRowNum = t.childNodes.length - 1; // not a very reliable way of getting rownumber
-        for (i=0; i<nv; i++) {
+        for (i=0; i<nv; i++) 
+        {
             if (literalNodeRC(lastRowNum, i)) 
                 td = createNonLiteralNode();
             else 
@@ -304,13 +341,15 @@ function tableResize_OnMouseUp(event) {
         t.appendChild(tr);
     }
     
-    function createLiteralNode() {
+    function createLiteralNode() 
+    {
         var td = document.createElement("TD");
-        td.innerHTML = "Enter Text";
+        td.innerHTML = " ";
         return td;
     }
     
-    function createNonLiteralNode() {
+    function createNonLiteralNode() 
+    {
         var td = document.createElement("TD");
         td.setAttribute('about', '');
         td.setAttribute('style', 'color:#4444ff');
@@ -331,6 +370,7 @@ function tableResize_OnMouseUp(event) {
         var TDNode = t.childNodes[row].childNodes[col];
         if (literalNodeTD (TDNode)) return true;
     }
+    //--------------- End Add Row -----------------//
 
     function drawExport () {
         var form= this.document.createElement('form');
@@ -381,22 +421,22 @@ function tableDoubleClick(event) {
 function exportTable()
 {
     /*sel=document.getElementById('exportType')
-	var type = sel.options[sel.selectedIndex].value
-	
-	switch (type)
-	{
-		case 'cv':
+    var type = sel.options[sel.selectedIndex].value
+    
+    switch (type)
+    {
+        case 'cv':
 
-			break;
-		case 'html':
+            break;
+        case 'html':
 */
-	var win=window.open('table.html','Save table as HTML');
+    var win=window.open('table.html','Save table as HTML');
     var tbl=this.document.getElementById('tabulated_data');
     win.document.write('<TABLE>');
     for(j=0;j<tbl.childNodes[0].childNodes.length;j++)
     {
         win.document.write('<TH>'+ts_getInnerText(tbl.childNodes[0].cells[j])
-			   +'</TH>')
+            +'</TH>')
     }
     for(i=1;i<tbl.childNodes.length;i++)
     {
@@ -416,18 +456,18 @@ function exportTable()
     win.document.write('</TABLE>');
     win.document.uri='table.html'
     win.document.close();
-    /*			break;
-	   	case 'sparql':
-			//makeQueryLines();
-			var spr = document.getElementById('SPARQLText')
-			spr.setAttribute('class','expand')
+    /*          break;
+        case 'sparql':
+            //makeQueryLines();
+            var spr = document.getElementById('SPARQLText')
+            spr.setAttribute('class','expand')
             document.getElementById('SPARQLTextArea').value=queryToSPARQL(myQuery);
-			//SPARQLToQuery("PREFIX ajar: <http://dig.csail.mit.edu/2005/ajar/ajaw/data#> SELECT ?v0 ?v1 WHERE { ajar:Tabulator <http://usefulinc.com/ns/doap#developer> ?v0 . ?v0 <http://xmlns.com/foaf/0.1/birthday> ?v1 . }")
-			//matrixTable(myQuery, sortables_init)
-    		      //sortables_init();
-			break;
-		case '': 
-			alert('Please select a file type');
-			break;
-	}*/
+            //SPARQLToQuery("PREFIX ajar: <http://dig.csail.mit.edu/2005/ajar/ajaw/data#> SELECT ?v0 ?v1 WHERE { ajar:Tabulator <http://usefulinc.com/ns/doap#developer> ?v0 . ?v0 <http://xmlns.com/foaf/0.1/birthday> ?v1 . }")
+            //matrixTable(myQuery, sortables_init)
+                //sortables_init();
+            break;
+        case '': 
+            alert('Please select a file type');
+            break;
+    }*/
 }

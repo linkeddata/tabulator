5e873c2cb8daa4b35ba87863399534278f17c66d
timbl <none@none>
2007-07-21 01:09:52 +0000

    Add XML serialization view of data documents

--- chrome/content/js/outline.js
+++ chrome/content/js/outline.js
@@ -395,7 +395,7 @@ function Outline(doc) {
         appendPropertyTRs(div, plist, false, withinDocumentPane.filter)
         return div    
     }
-    panes.register(withinDocumentPane);
+    // panes.register(withinDocumentPane);
     
     /*   Default Pane
     **
@@ -598,8 +598,6 @@ function Outline(doc) {
     ** or at least the RDF semantics we attribute to that resource,
     ** in generated N3 syntax.
     */
-
-
     n3Pane = {};
     n3Pane.icon = Icon.src.icon_n3Pane;
     n3Pane.label = function(subject) {
@@ -632,6 +630,45 @@ function Outline(doc) {
         return div
     }
     panes.register(n3Pane);
+
+    /*      RDF/XML content Pane
+    **
+    **  This pane shows the content of a particular RDF resource
+    ** or at least the RDF semantics we attribute to that resource,
+    ** in generated N3 syntax.
+    */
+    RDFXMLPane = {};
+    RDFXMLPane.icon = Icon.src.icon_RDFXMLPane;
+    RDFXMLPane.label = function(subject) {
+        var s = dataContentPane.label(subject);
+        if (!s) return null;
+        return s + ' as RDF/XML';
+    }
+
+    RDFXMLPane.render = function(subject) {
+        var div = myDocument.createElement("div")
+        div.setAttribute('class', 'RDFXMLPane');
+        // Because of smushing etc, this will not be a copy of the original source
+        // We could instead either fetch and re-parse the source,
+        // or we could keep all the pre-smushed triples.
+        var sts = kb.statementsMatching(undefined, undefined, undefined, subject); // @@ slow with current store!
+        /*
+        var kludge = kb.formula([]); // No features
+        for (var i=0; i< sts.length; i++) {
+            s = sts[i];
+            kludge.add(s.subject, s.predicate, s.object);
+        }
+        */
+        var sz = Serializer();
+        sz.suggestNamespaces(kb.namespaces);
+        sz.setBase(subject.uri);
+        var str = sz.statementsToXML(sts)
+        var pre = myDocument.createElement('PRE');
+        pre.appendChild(myDocument.createTextNode(str));
+        div.appendChild(pre);
+        return div
+    }
+    panes.register(RDFXMLPane);
 
     /*   Class member Pane
     **

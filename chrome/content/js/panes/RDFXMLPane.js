c5efb7876b4b031fb183ca3b6f8330d39e896f48
timbl <none@none>
2008-10-30 02:28:24 +0000

    Not done yet

---
+++ chrome/content/js/panes/RDFXMLPane.js
@@ -0,0 +1,47 @@
+
+
+    /*      RDF/XML content Pane
+    **
+    **  This pane shows the content of a particular RDF resource
+    ** or at least the RDF semantics we attribute to that resource,
+    ** in generated N3 syntax.
+    */
+tabulator.panes.register ({
+
+    icon: Icon.src.icon_RDFXMLPane,
+    
+    label: function(subject) {
+        var n = tabulator.kb.statementsMatching(
+            undefined, undefined, undefined, subject).length;
+        if (n == 0) return null;
+        return s + ' as RDF/XML';
+    }
+
+    render: function(subject, myDocument) {
+        var kb = tabulator.kb;
+        var div = myDocument.createElement("div")
+        div.setAttribute('class', 'RDFXMLPane');
+        // Because of smushing etc, this will not be a copy of the original source
+        // We could instead either fetch and re-parse the source,
+        // or we could keep all the pre-smushed triples.
+        var sts = kb.statementsMatching(undefined, undefined, undefined, subject); // @@ slow with current store!
+        /*
+        var kludge = kb.formula([]); // No features
+        for (var i=0; i< sts.length; i++) {
+            s = sts[i];
+            kludge.add(s.subject, s.predicate, s.object);
+        }
+        */
+        var sz = Serializer();
+        sz.suggestNamespaces(kb.namespaces);
+        sz.setBase(subject.uri);
+        var str = sz.statementsToXML(sts)
+        var pre = myDocument.createElement('PRE');
+        pre.appendChild(myDocument.createTextNode(str));
+        div.appendChild(pre);
+        return div
+    }
+}, false);
+
+// ends
+

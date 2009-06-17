3b28a700a3fdb19c41f74a52f97284a0dfc36e4f
cruz <none@none>
2009-06-17 15:43:38 +0000

    Updated publicationPane.js

--- chrome/content/js/panes/publicationPane.js
+++ chrome/content/js/panes/publicationPane.js
@@ -1,1 +1,40 @@
-/* Testing SVN */
+/*
+ * File: publicationPane.js
+ * Purpose: Pane that contains publication data, such as author(s), publishing date, location, etc.
+ * Author: cruz@mit.edu
+ */
+
+
+/*
+    URI NS to use: http://purl.oclc.org/NET/nknouf/ns/bibtex
+    Note: Mime-type is text/plain. Has funky issues when loaded by Firefox.
+*/
+
+tabulator.panes.register(tabulator.panes.publishingPane = {
+    name: 'Publication'
+
+    //icon: Icon.src.#something#,
+
+    bibtexNS: new RDFNamespace('http://purl.oclc.org/NET/nknouf/ns/bibtex#'),
+	
+    label: function(subject) {
+        if(tabulator.kb.whether(subject,tabulator.ns.owl('subClassOf'),this.bibtexNS('Entry')
+           || tabulator.kb.whether(subject,tabulator.ns.owl('class'),this.bibtexNS('Entry')))
+            return 'Publication';
+        else
+            return null;
+    },
+
+    render: function(subject, document) {
+        kb = tabulator.kb;
+
+        getAuthors = function(subject) {
+            authors = kb.statementsMatching(subject,this.bibtexNS('hasAuthor'))
+            if(authors == null) return false;
+            else return authors;
+        }
+
+        
+    }
+}, true);
+

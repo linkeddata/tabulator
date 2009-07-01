eee25db87d646a22b31e35377abd7b79f6625a44
cruz <none@none>
2009-07-01 22:38:28 +0000

    Fixed some patternSearch.js bugs

--- chrome/content/js/panes/patternSearch.js
+++ chrome/content/js/panes/patternSearch.js
@@ -2,14 +2,18 @@ function PatternSearch() { // Encapsulates all of the methods and classes
     /*****************************
      *     Main Node Class       *
      *****************************/
-    this.PatternNode = function(fetchMethod, searchMethod, children) {
-        this.fetchMethod = fetchMethod;
+    anchor = this;
+    this.PatternNode = function(searchMethod, fetchMethod, children) {
         this.searchMethod = searchMethod;
+        this.fetchMethod = fetchMethod;
         this.children = children;
 
-        this.fetch = new function(subject) {
-            if(subjects == null) return null;
-            return this.fetchMethod(this.searchMethod(subjects), children);
+        this.fetch = function(subject) {
+            var newTerm = AJAR_handleNewTerm(tabulator.kb, subject, subject);
+            if(subject == null) return null;
+            searchM = this.searchMethod(subject);
+            fetchM = this.fetchMethod(searchM, children);
+            return fetchM;
         }   
     }
 
@@ -20,6 +24,7 @@ function PatternSearch() { // Encapsulates all of the methods and classes
         if(subjects == null) return null;
         else if(subjects[0].termType === 'literal' || subjects[0].termType === 'symbol')
             return new DataContainer([subjects[0].toString()]);
+        else return null;
     }
 
     this.fetchMultiple = function(subjects) {
@@ -43,7 +48,7 @@ function PatternSearch() { // Encapsulates all of the methods and classes
         return null;
     }
 
-    fetchAnd = function(subjects, children) { // Children of And nodes must not return arrays
+    this.fetchAnd = function(subjects, children) { // Children of And nodes must not return arrays
         if(subjects == null || children == null) return null;
         for(var i = 0;i < children.length;i++) {
             var dataContainer = new DataContainer();
@@ -58,11 +63,11 @@ function PatternSearch() { // Encapsulates all of the methods and classes
         return null;
     }
 
-    fetchMultipleOr = function(subjects, children) {
+    this.fetchMultipleOr = function(subjects, children) {
         if(subjects == null || children == null) return null;
         var dataContainers = new Array();
         for(var i = 0;i < children.length;i++) {
-            fetchedData = this.fetchSingleOr(subjects, children);
+            fetchedData = anchor.fetchSingleOr(subjects, children); //TODO
             if(fetchedData == null) continue;
             else if(fetchedData.data) dataContainers.push(fetchedData);
             else if(fetchedData.length) // In case one child returns multiple DataContainers
@@ -92,7 +97,7 @@ function PatternSearch() { // Encapsulates all of the methods and classes
             var matchingTriples = new Array();
             if(isEmpty(triples)) return matchingTriples;
             for(var i = 0;i < objects.length;i++)
-                if(tabulator.kb.whether(objects[i],tabulator.ns.rdf('type'),desiredType))
+                if(tabulator.kb.whether(objects[i],tabulator.ns.rdf('type'),uri))
                     matchingTriples.push(triples[i]);
             return getObjects(matchingTriples);
         }
@@ -106,14 +111,14 @@ function PatternSearch() { // Encapsulates all of the methods and classes
 
 
     /********************************
-     *      Utility Methods         *
+     *  Utility Methods & Classes   *
      ********************************/
     function DataContainer(data) {
         if(data == null || data == undefined) this.data = new Array();
         else this.data = data;
-        this.appendData = function(data) {
-            this.data.push(data);
-        }
+    }
+    DataContainer.prototype.appendData = function(data) {
+        this.data.push(data);
     }
 
     function isEmpty(array) {
@@ -129,4 +134,54 @@ function PatternSearch() { // Encapsulates all of the methods and classes
             objects.push(triples[i].object);
         return objects;
     }
+
+    // Returns an array of strings
+    //this.parseResults = function(results) {
+        
+
+
+    /**********************************
+     *   Shortcuts & Abbreviations    *
+     **********************************/
+    this.MultipleOrTypeNode = function(uri, children) {
+        return new this.PatternNode(this.searchByObjectType(uri),this.fetchMultipleOr,children);
+    }
+    this.MultipleOrPredicateNode = function(uri, children) {
+        return new this.PatternNode(this.searchByPredicate(uri),this.fetchMultipleOr,children);
+    }
+    this.MultipleTypeEndNode = function(uri) {
+        return new this.PatternNode(this.searchByObjectType(uri),this.fetchMultiple);
+    }
+    this.MultiplePredicateEndNode = function(uri) {
+        return new this.PatternNode(this.searchByPredicate(uri),this.fetchMultiple);
+    }
+    this.SingleOrTypeNode = function(uri, children) {
+        return new this.PatternNode(this.searchByObjectType(uri),this.fetchSingleOr,children);
+    }
+    this.SingleOrPredicateNode = function(uri, children) {
+        return new this.PatternNode(this.searchByPredicate(uri),this.fetchSingleOr,children);
+    }
+    this.SingleTypeEndNode = function(uri) {
+        return new this.PatternNode(this.searchByObjectType(uri),this.fetchSingle);
+    }
+    this.SinglePredicateEndNode = function(uri) {
+        return new this.PatternNode(this.searchByPredicate(uri),this.fetchSingle);
+    }
+    this.AndTypeNode = function(uri, children) {
+        return new this.PatternNode(this.searchByObjectType(uri),this.fetchAnd,children);
+    }
+    this.AndPredicateNode = function(uri, children) {
+        return new this.PatternNode(this.searchByPredicate(uri),this.fetchAnd,children);
+    }
+
+    this.MOTN = this.MultipleOrTypeNode;
+    this.MOPN = this.MultipleOrPredicateNode;
+    this.MTEN = this.MultipleTypeEndNode;
+    this.MPEN = this.MultiplePredicateEndNode;
+    this.SOTN = this.SingleOrTypeNode;
+    this.SOPN = this.SingleOrPredicateNode;
+    this.STEN = this.SingleTypeEndNode;
+    this.SPEN = this.SinglePredicateEndNode;
+    this.APN = this.AndPredicateNode;
+    this.ATN = this.AndTypeNode;
 }

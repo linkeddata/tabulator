629feaaa700cce9ac770a2edf68d29ba5ec0dcd3
lkagal@new-host-2.home <lkagal@new-host-2.home>
2009-12-19 18:34:18 -0500

    still removing hardcoded stuff from highlighting sidebar

--- chrome/content/highlightSidebar.js
+++ chrome/content/highlightSidebar.js
@@ -9,13 +9,6 @@ function highlightSidebar(doc) {
 	
 	var kb = tabulator.kb;		
 	
-	var ccLicenses = kb.statementsMatching(undefined, kb.sym("http://creativecommons.org/ns#license"), undefined);
-	var rmpLicenses = kb.statementsMatching(undefined, kb.sym("http://dig.csail.mit.edu/2008/02/rmp/rmp-schema#restricts"), undefined);
-
-	//dump("\n RMP: " + rmpLicenses);
-	//dump("\n CC: " + ccLicenses);
-	var licenses = ccLicenses + rmpLicenses;
-	dump("\n RMP + CC: " + licenses);
 
 	var policyURIArray =   ['http://creativecommons.org/licenses/by-sa/3.0/', 
 				'http://creativecommons.org/licenses/by/3.0/', 
@@ -29,17 +22,6 @@ function highlightSidebar(doc) {
 				'http://dig.csail.mit.edu/2008/02/rmp/rmp-schema#No-Financial',
 				'http://dig.csail.mit.edu/2008/02/rmp/rmp-schema#No-Medical'];
 	
-	//var policyLabelArray = ['CC:Attribution-ShareAlike: ', 'CC:Attribution: ', 'CC:Attribution-NoDerivs: ', 'CC:Attribution-NonCommercial-NoDerivs: ', 'CC:Attribution-NonCommercial: ', 'CC:Attribution-NonCommercial-ShareAlike: ', 'RMP:No-Commercial: ', 'RMP:No-Depiction: ', 'RMP:No-Employment: ', 'RMP:No-Financial: ', 'RMP:No-Medical: ' ];
-	
-	//var policySaveArray = ['ccBySa', 'ccBy', 'ccByNd', 'ccByNcNd', 'ccByNc', 'ccByNcSa', 'rmpCom', 'rmpDep', 'rmpEmp', 'rmpFin', 'rmpMed'];
-	
-	//var colorList = ['None', 'Gray', 'Blue', 'Red', 'Green', 'Magenta', 'Yellow', 'Purple', 'Teal', 'Aquamarine', 'Beige', 'Coral'];
-	
-	//var inUseArray = new Array(policyURIArray.length);
-	//for (var count = 0; count < inUseArray.length; count++) {
-	//	inUseArray[count] = 0;
-	//}
-					
 	var mainDiv = doc.getElementById('hlightdiv');
 	var mainBox = doc.createElement("groupbox");
 	mainBox.setAttribute('orient', 'vertical');
@@ -96,36 +78,55 @@ function highlightSidebar(doc) {
 		txtLabel.setAttribute('value', 'Licenses/Restrictions in Knowledge Base:');
 		txtDiv.appendChild(txtLabel);
 		newDiv.appendChild(txtDiv);
-		
-		// Loading preloaded stuff saved in tabulator.preferences
-		// loading any new licenses or restrictions found
-		for (var count = 0; count < policyURIArray.length; count++) {
-		//for (var count = 0; count < licenses.length; count++) {
-			//dump("\n License: " + licenses[count].object);
-			var loadColor = tabulator.preferences.get(policyURIArray[count]);
-			//var loadColor = tabulator.preferences.get(licenses[count].object);
-			//var inKB = kb.statementsMatching(undefined, undefined, kb.sym(licenses[count].object));
+
+		// list of all licenses and restrictions in the kb	
+		var ccLicenses = kb.statementsMatching(undefined, kb.sym("http://creativecommons.org/ns#license"), undefined);
+		var rmpLicenses = kb.statementsMatching(undefined, kb.sym("http://dig.csail.mit.edu/2008/02/rmp/rmp-schema#restricts"), undefined);
+
+		//dump("\n\n\n RMP: " + rmpLicenses);
+		//dump("\n CC: " + ccLicenses);
+		var license = rmpLicenses.concat(ccLicenses);
+		dump("\n RMP + CC: " + license);
+		var processedLicense = new Array();
+
+		// loading all licenses or restrictions found
+		//for (var count = 0; count < policyURIArray.length; count++) {
+		for (var count = 0; count < license.length; count++) {
+
+			var curLicense = license[count].object.uri;
+			dump("\n License: " + curLicense);
+
+			if (processedLicense[curLicense] != null) { continue }
+			//var loadColor = tabulator.preferences.get(policyURIArray[count]);
+
+			var loadColor = tabulator.preferences.get(curLicense);
+			var inKB = kb.statementsMatching(undefined, undefined, kb.sym(curLicense));
+			dump("\n inKB: " + inKB);
+
 			//var inKB = kb.statementsMatching(undefined, undefined, kb.sym(policyURIArray[count]));
-			var rmpKB = kb.statementsMatching(undefined, kb.sym("http://dig.csail.mit.edu/2008/02/rmp/rmp-schema#restricts"), kb.sym(policyURIArray[count]));
-			var ccKB = kb.statementsMatching(undefined, kb.sym("http://creativecommons.org/ns#license"), kb.sym(policyURIArray[count]));
-			var inKB = rmpKB + ccKB;
+			//var rmpKB = kb.statementsMatching(undefined, kb.sym("http://dig.csail.mit.edu/2008/02/rmp/rmp-schema#restricts"), kb.sym(policyURIArray[count]));
+			//var ccKB = kb.statementsMatching(undefined, kb.sym("http://creativecommons.org/ns#license"), kb.sym(policyURIArray[count]));
+			//var inKB = rmpKB + ccKB;
 
 			// clearing color setting for license/restriction not in KB
 			// added by LK 12/3/09
 			if ((loadColor!=null)&&(inKB.length==0)) { 
-			    tabulator.preferences.clear(policyURIArray[count]);
-			    //tabulator.preferences.clear(licenses[count].object);
+			    //tabulator.preferences.clear(policyURIArray[count]);
+			    tabulator.preferences.clear(curLicense);
           		}
 			if ((loadColor == null)&&(inKB.length > 0)) {
 				loadColor=getRandomColor();
-				tabulator.preferences.set(policyURIArray[count], loadColor);
-				//tabulator.preferences.set(licenses[count].object, loadColor);
+				//tabulator.preferences.set(policyURIArray[count], loadColor);
+				tabulator.preferences.set(curLicense, loadColor);
 			}
 			dump("\n preloaded color:" + loadColor + " count: " + count + " KB match:" + inKB);
+
 			if (inKB.length > 0) { 
-				//dupLicense[licenses[count].object] = "done";
-			        var policyLabel = kb.statementsMatching(kb.sym(policyURIArray[count]), kb.sym("http://www.w3.org/2000/01/rdf-schema#label"));
-			        //var policyLabel = kb.statementsMatching(kb.sym(licenses[count].object), kb.sym("http://www.w3.org/2000/01/rdf-schema#label"));
+				// setting processedLicense	
+				processedLicense[curLicense] = "done";
+
+			        //var policyLabel = kb.statementsMatching(kb.sym(policyURIArray[count]), kb.sym("http://www.w3.org/2000/01/rdf-schema#label"));
+			        var policyLabel = kb.statementsMatching(kb.sym(curLicense), kb.sym("http://www.w3.org/2000/01/rdf-schema#label"));
 				dump("\n Policy label: " + policyLabel);
 				//inUseArray[count] = 1;
 				var txtDiv = doc.createElement("div");
@@ -134,7 +135,8 @@ function highlightSidebar(doc) {
 
 				//txtLabel.setAttribute('value', policyLabelArray[count]);
 				txtLabel.setAttribute('value', policyLabel[0].object);
-				txtLabel.setAttribute('href', policyURIArray[count]);
+				//txtLabel.setAttribute('href', policyURIArray[count]);
+				txtLabel.setAttribute('href', curLicense);
 				txtDiv.appendChild(txtLabel);
 
 				// modified by Lk 12/18/09
@@ -151,71 +153,7 @@ function highlightSidebar(doc) {
 			}	
 		}
 
-	
-		/******* Add same functionality for CC licenses *******/
-		/*
-		for (var count = 0; count < ccLicenses.length; count++) {
-			if (inUseArray[policyURIArray.indexOf(ccLicenses[count].object.uri)] == 0) {
-				// unsetting inUseArray added by LK 12/3/09
-                                inUseArray[policyURIArray.indexOf(ccLicenses[count].object.uri)] = -1;
-				var txtDiv = doc.createElement("div");
-				txtDiv.setAttribute('class', 'colorTab');
-				var txtLabel = doc.createElement("label");
-
-				if (policyURIArray.indexOf(ccLicenses[count].object.uri) > -1) {
-					txtLabel.setAttribute('value', policyLabelArray[policyURIArray.indexOf(ccLicenses[count].object.uri)]);
-				}
-
-				txtDiv.appendChild(txtLabel);
-
-				// modified by Lk 12/18/09
-				// adding colorpicker control
-                                var colorInput = doc.createElement("colorpicker");
-                                colorInput.setAttribute('type', 'button');
-                                colorInput.setAttribute('color', loadColor);
-				colorInput.addEventListener("click", function() {
-                                        saveSettings();
-                                } , false);
-                                txtDiv.appendChild(colorInput);
-                                newDiv.appendChild(txtDiv);
-
-			}
-		} */
-		
-		/******* Add same functionality for RMP restrictions *******/
-		/*
-		for (var count = 0; count < rmpLicenses.length; count++) {
-			if (inUseArray[policyURIArray.indexOf(rmpLicenses[count].object.uri)] == 0) {		
-				// unsetting inUseArray added by LK 12/3/09
-                                inUseArray[policyURIArray.indexOf(rmpLicenses[count].object.uri)] = -1;
-				var txtDiv = doc.createElement("div");
-				txtDiv.setAttribute('class', 'colorTab');
-				var txtLabel = doc.createElement("label");
-
-				if (policyURIArray.indexOf(rmpLicenses[count].object.uri) > -1) {
-					txtLabel.setAttribute('value', policyLabelArray[policyURIArray.indexOf(rmpLicenses[count].object.uri)]);
-				}
-
-				txtDiv.appendChild(txtLabel);
-			
-				// modified by Lk 12/18/09
-				// adding colorpicker control
-                                var colorInput = doc.createElement("colorpicker");
-                                colorInput.setAttribute('type', 'button');
-                                colorInput.setAttribute('color', loadColor);
-				colorInput.addEventListener("click", function() {
-                                        saveSettings();
-                                } , false);
-                                txtDiv.appendChild(colorInput);
-                                newDiv.appendChild(txtDiv);
-	
-			}
-		}*/	
-
-		
-
 		div.appendChild(newDiv);
-	
 	}	
 
 	/*	

0160e7a71e5f9e4e4e8ec3a18ed2592307e97d6a
lkagal@new-host-2.home <lkagal@new-host-2.home>
2009-12-18 21:29:24 -0500

    saving color settings on colorpicker's click

--- chrome/content/highlightSidebar.js
+++ chrome/content/highlightSidebar.js
@@ -47,7 +47,9 @@ function highlightSidebar(doc) {
 		for (var count = 1; count < saveDiv.childNodes.length; count++) {
 			var policyDiv = saveDiv.childNodes[count];
 			var saveName = policySaveArray[policyLabelArray.indexOf(policyDiv.childNodes[0].getAttribute('value'))];
-			var color = policyDiv.childNodes[1].getAttribute('value');
+			// modified by LK 12/18/09
+			//var color = policyDiv.childNodes[1].getAttribute('value');
+			var color = policyDiv.childNodes[1].getAttribute('color');
 			tabulator.preferences.set(saveName, color);	
 		}
 		
@@ -78,13 +80,17 @@ function highlightSidebar(doc) {
 		for (var count = 0; count < policySaveArray.length; count++) {
 			var loadColor = tabulator.preferences.get(policySaveArray[count]);
 			var inKB = kb.statementsMatching(undefined, undefined, kb.sym(policyURIArray[count]));
+			dump("\n preloaded color:" + loadColor + " count: " + count + " KB match:" + inKB);
 
-			// clearing color setting not in KB
+			// clearing color setting for license/restriction not in KB
 			// added by LK 12/3/09
-			if ((loadColor)&&(inKB.length==0)) { 
+			if ((loadColor!=null)&&(inKB.length==0)) { 
 			    tabulator.preferences.clear(policySaveArray[count]);
           		}
-			if ((loadColor)&&(inKB.length > 0)) { 
+			if (loadColor == null) {
+				loadColor="Black";
+			}
+			if (inKB.length > 0) { 
 				inUseArray[count] = 1;
 				var txtDiv = doc.createElement("div");
 				txtDiv.setAttribute('class', 'colorTab');
@@ -93,28 +99,17 @@ function highlightSidebar(doc) {
 				txtLabel.setAttribute('value', policyLabelArray[count]);
 				txtDiv.appendChild(txtLabel);
 
+				// modified by Lk 12/18/09
+				// adding colorpicker control
 				var colorInput = doc.createElement("colorpicker");
 				colorInput.setAttribute('type', 'button');
 				colorInput.setAttribute('color', loadColor);
+				colorInput.addEventListener("click", function() {
+                			saveSettings();
+                		} , false);
 				txtDiv.appendChild(colorInput);
 				newDiv.appendChild(txtDiv);
 
-				var colMenu = doc.createElement("menulist");
-				colMenu.setAttribute('id', 'colMenu ');
-				colMenu.setAttribute('onmousedown', '"show()"');
-
-				var colMenuhelper = doc.createElement("menupopup");
-				for (var i = 0; i < colorList.length; i++) {
-					var colOption = doc.createElement("menuitem");
-					colOption.setAttribute('value', colorList[i]);
-					colOption.setAttribute('label', colorList[i]);
-					if (loadColor == colorList[i])
-						colOption.setAttribute('selected', true);
-					colMenuhelper.appendChild(colOption);
-				}
-				colMenu.appendChild(colMenuhelper);
-				txtDiv.appendChild(colMenu);
-				newDiv.appendChild(txtDiv); 
 			}	
 		}
 
@@ -127,27 +122,23 @@ function highlightSidebar(doc) {
 				txtDiv.setAttribute('class', 'colorTab');
 				var txtLabel = doc.createElement("label");
 
-				if (policyURIArray.indexOf(ccLicenses[count].object.uri) > -1)
-				{
+				if (policyURIArray.indexOf(ccLicenses[count].object.uri) > -1) {
 					txtLabel.setAttribute('value', policyLabelArray[policyURIArray.indexOf(ccLicenses[count].object.uri)]);
 				}
 
 				txtDiv.appendChild(txtLabel);
 
-				var colMenu = doc.createElement("menulist");
-				colMenu.setAttribute('id', 'colMenu ');
-				colMenu.setAttribute('onmousedown', '"show()"');
+				// modified by Lk 12/18/09
+				// adding colorpicker control
+                                var colorInput = doc.createElement("colorpicker");
+                                colorInput.setAttribute('type', 'button');
+                                colorInput.setAttribute('color', loadColor);
+				colorInput.addEventListener("click", function() {
+                                        saveSettings();
+                                } , false);
+                                txtDiv.appendChild(colorInput);
+                                newDiv.appendChild(txtDiv);
 
-				var colMenuhelper = doc.createElement("menupopup");
-				for (var i = 0; i < colorList.length; i++) {
-					var colOption = doc.createElement("menuitem");
-					colOption.setAttribute('value', colorList[i]);
-					colOption.setAttribute('label', colorList[i]);	
-					colMenuhelper.appendChild(colOption);
-				}
-				colMenu.appendChild(colMenuhelper);
-				txtDiv.appendChild(colMenu);
-				newDiv.appendChild(txtDiv);	
 			}
 		}
 		
@@ -164,27 +155,23 @@ function highlightSidebar(doc) {
 				txtDiv.setAttribute('class', 'colorTab');
 				var txtLabel = doc.createElement("label");
 
-				if (policyURIArray.indexOf(rmpLicenses[count].object.uri) > -1)
-				{
+				if (policyURIArray.indexOf(rmpLicenses[count].object.uri) > -1) {
 					txtLabel.setAttribute('value', policyLabelArray[policyURIArray.indexOf(rmpLicenses[count].object.uri)]);
 				}
 
 				txtDiv.appendChild(txtLabel);
-
-				var colMenu = doc.createElement("menulist");
-				colMenu.setAttribute('id', 'colMenu ');
-				colMenu.setAttribute('onmousedown', '"show()"');
-
-				var colMenuhelper = doc.createElement("menupopup");
-				for (var i = 0; i < colorList.length; i++) {
-					var colOption = doc.createElement("menuitem");
-					colOption.setAttribute('value', colorList[i]);
-					colOption.setAttribute('label', colorList[i]);	
-					colMenuhelper.appendChild(colOption);
-				}
-				colMenu.appendChild(colMenuhelper);
-				txtDiv.appendChild(colMenu);
-				newDiv.appendChild(txtDiv);
+			
+				// modified by Lk 12/18/09
+				// adding colorpicker control
+                                var colorInput = doc.createElement("colorpicker");
+                                colorInput.setAttribute('type', 'button');
+                                colorInput.setAttribute('color', loadColor);
+				colorInput.addEventListener("click", function() {
+                                        saveSettings();
+                                } , false);
+                                txtDiv.appendChild(colorInput);
+                                newDiv.appendChild(txtDiv);
+	
 			}
 		}		
 
@@ -201,8 +188,7 @@ function highlightSidebar(doc) {
 		{
 			var loadColor = tabulator.preferences.get(policySaveArray[count]);
 
-			if (loadColor)
-			{			
+			if (loadColor) {			
 				inUseArray[count] = 1;
 				var txtDiv = doc.createElement("div");
 				txtDiv.setAttribute('class', 'colorTab');
@@ -211,20 +197,17 @@ function highlightSidebar(doc) {
 				txtLabel.setAttribute('value', policyLabelArray[count]);
 				txtDiv.appendChild(txtLabel);
 
-				var colMenu = doc.createElement("menulist");
-				colMenu.setAttribute('id', 'colMenu ');
-				colMenu.setAttribute('onmousedown', '"show()"');
-				var colMenuhelper = doc.createElement("menupopup");
-				for (var i = 0; i < colorList.length; i++) {
-					var colOption = doc.createElement("menuitem");
-					colOption.setAttribute('value', colorList[i]);
-					colOption.setAttribute('label', colorList[i]);	
-					colMenuhelper.appendChild(colOption);
-				}
-				colMenu.appendChild(colMenuhelper);
-				txtDiv.appendChild(colMenu);
-				alert('preload');
-				loadDiv.appendChild(txtDiv);
+				// modified by Lk 12/18/09
+				// adding colorpicker control
+                                var colorInput = doc.createElement("colorpicker");
+                                colorInput.setAttribute('type', 'button');
+                                colorInput.setAttribute('color', loadColor);
+				colorInput.addEventListener("click", function() {
+                                        saveSettings();
+                                } , false);
+                                txtDiv.appendChild(colorInput);
+                                loadDiv.appendChild(txtDiv);
+
 			}
 			alert('here'); 
 		}
@@ -238,23 +221,17 @@ function highlightSidebar(doc) {
 	var tmp = doc.createElement("label");
 	tmp.setAttribute('value', 'Welcome to the License/Restriction Highlighter!');
 	infoBox.appendChild(tmp);
-	
-	var buttonDiv = doc.createElement("div");	
-	var saveButton = doc.createElement("button");
-	saveButton.setAttribute('label', 'Save Highlighting Options');
-	saveButton.addEventListener("click", function() {
-		saveSettings();
-		} , false);
-	buttonDiv.appendChild(saveButton);	
+
+	// commented out LK 12/18/09	
+	//var buttonDiv = doc.createElement("div");	
+	//var saveButton = doc.createElement("button");
+	//saveButton.setAttribute('label', 'Save Highlighting Options');
+	//saveButton.addEventListener("click", function() {
+	//	saveSettings();
+	//	} , false);
+	//buttonDiv.appendChild(saveButton);	
         doc.addEventListener("load",function() {highlightSidebar();},false);
 	
-	//var removeButton = doc.createElement("button");
-	//removeButton.setAttribute('label', 'Remove Highlighting Options');
-	//removeButton.addEventListener("click", function() {
-	//	removeSettings();
-	//	} , false);
-	//buttonDiv.appendChild(removeButton);	
-
 	mainBox.appendChild(infoBox);
 		
 	createKBHighlighter(mainBox);

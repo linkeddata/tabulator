5856fdd36515741fef46e0ca8462e46582907117
Tim Berners-Lee <timbl+hg@w3.org>
2010-08-19 11:53:32 -0400

    Add tableViewPane.js with high priority for classes
    --HG--
    branch : refactor

--- chrome/content/js/init/panes.js
+++ chrome/content/js/init/panes.js
@@ -12,7 +12,6 @@ tabulator.panes.paneForPredicate = []
 tabulator.panes.register = function(p, whether) {
     p.requireQueryButton = whether;
     tabulator.panes.list.push(p);
-    // alert("Registering pane "+p.name); //@@
     if (p.icon) tabulator.panes.paneForIcon[p.icon] = p;
     if (p.predicates) {
         for (x in p.predicates) {
@@ -36,12 +35,11 @@ loader.loadSubScript("chrome://tabulator/content/js/panes/dataContentPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/airPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/n3Pane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/RDFXMLPane.js");
+loader.loadSubScript("chrome://tabulator/content/js/panes/tableViewPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/classInstancePane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/defaultPane.js");
 //loader.loadSubScript("chrome://tabulator/content/js/panes/newOutline.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/categoryPane.js");
-loader.loadSubScript("chrome://tabulator/content/js/panes/internalPane.js");
-loader.loadSubScript("chrome://tabulator/content/js/panes/paneUtils.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/pubsPane.js");
 
 //@@ jambo commented these things out to pare things down temporarily.
@@ -52,13 +50,12 @@ loader.loadSubScript("chrome://tabulator/content/js/panes/RDFXMLPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/humanReadablePane.js");
 */
 loader.loadSubScript("chrome://tabulator/content/js/panes/microblogPane/microblogPane.js");
-/*
-loader.loadSubScript("chrome://tabulator/content/js/panes/imagePane.js");
-loader.loadSubScript("chrome://tabulator/content/js/panes/tableViewPane.js");
-*/
+
+// loader.loadSubScript("chrome://tabulator/content/js/panes/imagePane.js");
+
 loader.loadSubScript("chrome://tabulator/content/js/panes/socialPane.js");
 /*loader.loadSubScript("chrome://tabulator/content/js/panes/social/pane.js");
-/*loader.loadSubScript("chrome://tabulator/content/js/panes/airPane.js");
+loader.loadSubScript("chrome://tabulator/content/js/panes/airPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/lawPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/pushbackPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/CVPane.js");
@@ -66,3 +63,9 @@ loader.loadSubScript("chrome://tabulator/content/js/panes/photoPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/tagPane.js");
 loader.loadSubScript("chrome://tabulator/content/js/panes/photoImportPane.js");
 */
+// The internals pane is always the last as it is the least user-friendly
+loader.loadSubScript("chrome://tabulator/content/js/panes/internalPane.js");
+loader.loadSubScript("chrome://tabulator/content/js/panes/paneUtils.js");
+
+// ENDS
+

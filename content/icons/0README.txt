b9b89051bac5f6616b19b382eacba75c0700753d
Joe Presbrey <presbrey+git@gmail.com>
2012-02-21 17:01:19 +0000

    merge: sync timbl

--- content/icons/0README.txt
+++
@@ -1,5 +0,0 @@
-
-One site with usable icons -- there are many
-
-http://icons.mysitemyway.com/
-
--- content/test/tc0007/node-test.js
+++
@@ -1,5 +0,0 @@
-
-// See npm and node.js documentstion http://nodejs.org/docs/v0.6.5/api/modules.html
-//
-var jQuery = require('jquery');
-var $rdf = require('../../js/rdf/dist/node-rdflib.js');
